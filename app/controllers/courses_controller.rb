018f1c3eaf2217b552320f1f6928a63a53218954
TMad <madsen.tyler@gmail.com>
2017-12-13 15:19:41 -0700

    add course db, controller, view, model

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,26 @@
+class CoursesController < ApplicationController
+
+  def show
+    @course = Course.all
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @student.save
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name)
+  end
+
+end
