077a1ad2ec3080e1e09dfe4a72958ba36413166a
TMad <madsen.tyler@gmail.com>
2017-12-13 13:34:41 -0700

    add new, edit, and index functionality

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,42 @@
+class StudentsController < ApplicationController
+
+    before_action :set_student, only: [:show, :edit, :update]
+
+    def index
+      @students = Student.all
+    end
+
+    def new
+      @student = Student.new
+    end
+
+    def create
+      @student = Student.new(student_params)
+      if @student.save
+        flash[:success] = "Successfully created #{@student.name}!"
+        redirect_to student_path(@student)
+      else
+        render :new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      @student.update(student_params)
+    end
+
+    def show
+    end
+
+    private
+
+    def student_params
+      params.require(:student).permit(:name)
+    end
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
+end
